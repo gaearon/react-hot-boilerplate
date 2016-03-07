662728ae82d85e97fe0f4cefc9f99f7e5bbbdb6c
aoto <aoto_yu@163.com>
2016-03-07 15:22:23 +0800

    Add css hot replacement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './App.css';
+
 export default class App extends Component {
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,17 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/, 
+        loaders: ['style', 'css'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
   }
 };
