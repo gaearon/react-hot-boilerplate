b52c727937a499f3efdc5dceb74ae952aa318c3a
Dan Abramov <dan.abramov@gmail.com>
2016-04-30 18:00:43 +0100

    Bump to 3.0.0-beta.0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Layout from './Layout';
 import Counter from './Counter';
 
+// If you use React Router, make this component
+// render <Router> with your routes. Currently,
+// only synchronous routes are hot reloaded, and
+// you will see a warning from <Router> on every reload.
+// You can ignore this warning. For details, see:
+// https://github.com/reactjs/react-router/issues/2182
+
 export default class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import App from './App';
 
 const rootEl = document.getElementById('root');
 ReactDOM.render(
-  <AppContainer component={App} />,
+  <AppContainer>
+    <App />
+  </AppContainer>,
   rootEl
 );
 
 if (module.hot) {
   module.hot.accept('./App', () => {
+    // If you use Webpack 2 in ES modules mode, you can
+    // use <App /> here rather than require() a <NextApp />.
+    const NextApp = require('./App').default;
     ReactDOM.render(
-      <AppContainer component={require('./App').default} />,
+      <AppContainer>
+         <NextApp />
+      </AppContainer>,
       rootEl
     );
   });
