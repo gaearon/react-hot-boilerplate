821c3425ffef1e4769b5b5c7460d6258923b708d
Brad Daily <bdaily@etsy.com>
2016-05-20 18:04:39 -0400

    Use webpack middleware

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+var path = require('path');
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
+var express = require('express');
+var devMiddleware = require('webpack-dev-middleware');
+var hotMiddleware = require('webpack-hot-middleware');
+
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
+var app = express();
+var compiler = webpack(config);
+
+app.use(devMiddleware(compiler, {
   publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+  historyApiFallback: true,
+}));
+
+app.use(hotMiddleware(compiler));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, function(err) {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
 
   console.log('Listening at http://localhost:3000/');
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   devtool: 'eval',
   entry: [
     'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack-hot-middleware/client',
     './src/index'
   ],
   output: {
