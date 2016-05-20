5c76efd2d04b2d116cf96c2cee58e10765dbab0b
Brad Daily <bdaily@etsy.com>
2016-05-20 18:17:13 -0400

    Formatting

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 var express = require('express');
 var devMiddleware = require('webpack-dev-middleware');
 var hotMiddleware = require('webpack-hot-middleware');
-
 var config = require('./webpack.config');
 
 var app = express();
@@ -20,7 +19,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function(err) {
+app.listen(3000, function (err) {
   if (err) {
     return console.error(err);
   }
