3b637e2f8805aea17b054d66631d1617ed8d0e9e
Dave Marr <davemarr@gmail.com>
2016-05-23 21:50:30 -0700

    Prefer middleware over dev-server

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+var path = require('path');
+
+var express = require('express');
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+var app = express();
+
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, 'localhost', function (err) {
   if (err) {
-    return console.log(err);
+    console.log(err);
+    return;
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:3000');
 });
