7673e726759bd39fd5bd05240ecbfee4dd236741
Michael Dauz <michaeldauz@gmail.com>
2016-07-21 11:04:10 -0700

    initial firebase packages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// Firebase
+import Rebase  from 're-base';
+var base = Rebase.createClass('https://dekuazim.firebaseio.com/');
 
 export default class App extends Component {
   render() {
