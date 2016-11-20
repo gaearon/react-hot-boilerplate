9089b6b83d8320110aab09a36f0bbfed0f9813fa
Cale Newman <newman.cale@gmail.com>
2016-11-20 17:40:30 -0600

    use cheap-module-eval-source-map option for source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'cheap-module-eval-source-map',
   entry: [
     'react-hot-loader/patch',
     'webpack-hot-middleware/client',
