26938f2efa40dd48e8f1a165d923fea0d00bf021
Cale Newman <newman.cale@gmail.com>
2017-01-25 16:41:58 -0600

    update license and fix some formatting

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ if (module.hot) {
     const NextApp = require('./App').default;
     ReactDOM.render(
       <AppContainer>
-         <NextApp />
+        <NextApp />
       </AppContainer>,
       rootEl
     );
