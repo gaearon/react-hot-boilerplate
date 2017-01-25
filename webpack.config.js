314c024ba33bb18dba86f5d69ae08b57691146a3
= <petertrotman@gmail.com>
2017-01-25 14:53:58 +0000

    Updated .babelrc and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,66 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
   entry: [
     'react-hot-loader/patch',
-    'webpack-hot-middleware/client',
-    './src/index'
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:3000',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './src/index',
+    // the entry point of our app
   ],
+
   output: {
-    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+    // the output bundle
+
+    path: path.resolve(__dirname, 'dist'),
+
     publicPath: '/static/'
+    // necessary for HMR to know where to load the hot update chunks
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-  ],
+
+  context: path.resolve(__dirname, 'src'),
+  devtool: 'inline-source-map',
+
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+  ],
+
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: resolve(__dirname, 'dist'),
+    // match the output path
+
+    publicPath: '/static/'
+    // match the output `publicPath`
+  },
 };
